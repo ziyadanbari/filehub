d86bae1b51656a240ce4885d5f5a307661c538e4
Amine Anbari <amine.a@wiggli.io>
2024-08-08 14:47:29 +0100

    add app url to cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://filehub-navy.vercel.apps'],
+    'allowed_origins' => ['http://localhost:3000', 'https://filehub-navy.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
